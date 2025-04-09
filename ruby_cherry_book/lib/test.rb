542b815d47a0704e804ef5ddf204f0f4e0cc6398
taka4655 <taku.enginner@gmail.com>
2025-04-09 13:22:16 +0900

    case文でwhen節に複数の値を指定する

--- ruby_cherry_book/lib/test.rb
+++ ruby_cherry_book/lib/test.rb
@@ -1,29 +1,29 @@
-# 1つのオブジェクトや式を複数の値と比較する場合
-# elsifを重ねるより、caseで書いた方がシンプル。
+# case文でwhen説に複数の値を指定し、どれかに一致すれば処理を実行する
 
-country = "italy"
-
-# if文を使う場合
-greeting = 
-  if country == "japan"
-    "こんにちは"
-  elsif country == "us"
-    "hello"
-  elsif country == "italy"
-    "ciao"
-  else
-    "???"
-  end
+country = "アメリカ"
+greeting = case country
+when "japan", "日本"
+  "こんにちは"
+when "us", "アメリカ"
+  "hello"
+when "italy", "イタリア"
+  "ciao"
+else
+  "???"
+end
 
 puts greeting
 
-# case文を使う場合
-greeting = case country
-when "japan"
+# case節の式を省略すると、条件式が最初に真になるwhen節の処理が実行される
+country = "italy"
+
+# case節の式を省略し、when節の条件式を順に評価するcase文
+greeting = case
+when country == "japan"
   "こんにちは"
-when "us"
+when country == "us"
   "hello"
-when "italy"
+when country == "italy"
   "ciao"
 else
   "???"
