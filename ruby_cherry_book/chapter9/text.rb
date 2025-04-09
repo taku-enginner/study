fdafe6ab036661278317b1f9bd4e3a22c974d0a9
taka4655 <taku.enginner@gmail.com>
2025-04-09 16:40:36 +0900

    意図的に例外を発生させる

--- ruby_cherry_book/chapter9/text.rb
+++ ruby_cherry_book/chapter9/text.rb
@@ -1,22 +1,62 @@
-# # 例外発生時にやり直したいときはretry
-# begin
-#   # 例外が発生するかもしれない処理  
-# rescue
-#   retry # 処理をやり直す  
+# 想定外のデータが来たときに意図的に例外を発生させる
+
+# def currency_of(country)
+#   case country
+#   when :japan
+#     "yen"
+#   when :us
+#     "dollar"
+#   when :india
+#     "rupee"
+#   else
+#     # 意図的に例外を発生させる
+#     raise "無効な国名です。#{country}" # 文字列がエラーメッセージになる
+#   end
 # end
 
-# リトライするときは回数上限を意識する
-retry_count = 0
-begin
-  puts "処理を開始します"
-  # わざと例外を発生させる
-  1 / 0
-rescue
-  retry_count += 1
-  if retry_count <= 3
-    puts "retryします。（#{retry_count}回目）"
-    retry
+# currency_of(:japan)
+# currency_of(:italy)
+
+# [taku_enginner@DESKTOP-VI1F9G6:chapter9(master)] $ ruby text.rb 
+# text.rb:13:in `currency_of': 無効な国名です。italy (RuntimeError)
+#         from text.rb:18:in `<main>'
+
+# raiseメソッドに文字列だけを渡したときはRuntimeErrorクラスの例外が発生する。
+# 第一引数に例外クラス、第二引数にエラーメッセージを渡すと、RuntimeErrorクラス以外の例外クラスを発生させることができる。
+# def currency_of(country)
+#   case country
+#   when :japan
+#     "yen"
+#   when :us
+#     "dollar"
+#   when :india
+#     "rupee"
+#   else
+#     # 意図的に例外を発生させる
+#     raise ArgumentError, "無効な国名です。#{country}" # 文字列がエラーメッセージになる
+#   end
+# end
+
+# currency_of(:japan)
+# currency_of(:italy)
+
+
+# raiseメソッドに例外クラスのインスタンスを渡す方法もある
+def currency_of(country)
+  case country
+  when :japan
+    "yen"
+  when :us
+    "dollar"
+  when :india
+    "rupee"
   else
-    puts "retryに失敗しました。"
+    # raiseメソッドに例外クラスのインスタンスを渡す
+    raise ArgumentError.new("無効な国名です。#{country}")  # 文字列がエラーメッセージになる
   end
 end
+
+currency_of(:japan)
+currency_of(:italy)
+
+# メッセージ省略はデバッグがしにくくなるため通常は避けるべき。
