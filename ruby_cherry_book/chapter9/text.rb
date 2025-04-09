53d2ea800026cf3c504700122eaaa6631d1027ca
taka4655 <taku.enginner@gmail.com>
2025-04-09 15:54:08 +0900

    例外処理の基本であるbegin~end

--- ruby_cherry_book/chapter9/text.rb
+++ ruby_cherry_book/chapter9/text.rb
@@ -9,11 +9,24 @@
 #         from /home/taku_enginner/.rbenv/versions/3.0.1/bin/irb:23:in `load'
 #         from /home/taku_enginner/.rbenv/versions/3.0.1/bin/irb:23:in `<main>'
 
-puts "Start."
-1 + "10"
-# 上の行で例外が発生するため、ここから下は実行されない
-puts "End."
+# puts "Start."
+# 1 + "10"
+# # 上の行で例外が発生するため、ここから下は実行されない
+# puts "End."
 
 # Start.
 # text.rb:13:in `+': String can't be coerced into Integer (TypeError)
 #         from text.rb:13:in `<main>'
+
+# 例外処理（これは例外処理としては良くない方法。できることとやっていいことは異なる。）
+puts "Start."
+
+# 例外処理を組み込んで例外に対処する
+begin
+  1 + "10"
+rescue
+   puts "例外が発生したが、このまま続行する"  
+end
+
+# 例外処理を組み込んだので、最後まで実行可能
+puts "End."
