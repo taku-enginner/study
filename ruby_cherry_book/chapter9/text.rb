6a1632bc7a514ae49b245205a0b0d019b7f8bbfd
taka4655 <taku.enginner@gmail.com>
2025-04-09 16:30:59 +0900

    継承関係とrescue節の順番に注意する

--- ruby_cherry_book/chapter9/text.rb
+++ ruby_cherry_book/chapter9/text.rb
@@ -1,9 +1,67 @@
-# rescue節に例外クラスを指定した場合、補足されるのはそのクラス自身とサブクラス。
-# Exceptionクラスを指定すると、StandardErrorと無関係のエラーまで補足することになる。
+# 例外クラスの継承関係とrescue節を各順番に注意しないと、永遠に実行されないrescue節を作ることになる。
 
-# 悪い例
+# 間違った例外処理の例
 begin
-  # 例外が起きそうな処理
-rescue Exception
-  # Exceptionとそのサブクラスが補足される。つまり、NoMemoryErrorやSystemExitまで補足される。  
+  # NoMethodErrorを発生させる
+  "abc".foo
+rescue NameError
+  # NoMethodErrorはここで補足される
+  puts "NameErrorです"
+rescue NoMethodError
+  # このrescue節は永遠に実行されない
+  puts "NoMethodErrorです"
+end
+
+# 正しい例
+begin
+  # NoMethodErrorを発生させる
+  "abc".foo
+rescue NoMethodError
+  # このrescue節は永遠に実行されない
+  puts "NoMethodErrorです"
+rescue NameError
+  # NoMethodErrorはここで補足される
+  puts "NameErrorです"
+end
+
+# 正しい例(NameError発生)
+begin
+  # NameErrorを発生させる
+  Foo.new
+rescue NoMethodError
+  # このrescue節は永遠に実行されない
+  puts "NoMethodErrorです"
+rescue NameError
+  # NoMethodErrorはここで補足される
+  puts "NameErrorです"
+end
+
+
+# 最後に書くとしたらStandardError
+begin
+  # NameErrorを発生させる
+  1 / 0
+rescue NoMethodError
+  # このrescue節は永遠に実行されない
+  puts "NoMethodErrorです"
+rescue NameError
+  # NoMethodErrorはここで補足される
+  puts "NameErrorです"
+rescue StandardError
+  puts "その他のエラーです"
+end
+
+
+# StandardErrorとそのサブクラスを補足するなら、そもそもクラスを指定する必要が無いので、省略するのがシンプル
+begin
+  # NameErrorを発生させる
+  1 / 0
+rescue NoMethodError
+  # このrescue節は永遠に実行されない
+  puts "NoMethodErrorです"
+rescue NameError
+  # NoMethodErrorはここで補足される
+  puts "NameErrorです"
+rescue # StandardErrorを指定しない
+  puts "その他のエラーです"
 end
