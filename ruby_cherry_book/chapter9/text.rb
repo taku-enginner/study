635c2499268be4a5a7dbc74847a6e2d2c2aa3513
taka4655 <taku.enginner@gmail.com>
2025-04-09 15:59:23 +0900

    例外発生時はメソッドをさかのぼる

--- ruby_cherry_book/chapter9/text.rb
+++ ruby_cherry_book/chapter9/text.rb
@@ -1,32 +1,35 @@
-# TypeErrorクラスの例外を発生
-# 2行目が例外が発生した場所を示す
-# 3行目以降はメソッドの呼び出し履歴（バックトレース、スタックトレースという）
+# method_1にだけ例外処理を記述する
+# 例外が発生した場所から1つずつ戻ってくる。
 
-# irb(main):001:0> 1 + "10"
-# (irb):1:in `+': String can't be coerced into Integer (TypeError)
-#         from (irb):1:in `<main>'
-#         from /home/taku_enginner/.rbenv/versions/3.0.1/lib/ruby/gems/3.0.0/gems/irb-1.3.5/exe/irb:11:in `<top (required)>'
-#         from /home/taku_enginner/.rbenv/versions/3.0.1/bin/irb:23:in `load'
-#         from /home/taku_enginner/.rbenv/versions/3.0.1/bin/irb:23:in `<main>'
-
-# puts "Start."
-# 1 + "10"
-# # 上の行で例外が発生するため、ここから下は実行されない
-# puts "End."
-
-# Start.
-# text.rb:13:in `+': String can't be coerced into Integer (TypeError)
-#         from text.rb:13:in `<main>'
-
-# 例外処理（これは例外処理としては良くない方法。できることとやっていいことは異なる。）
-puts "Start."
-
-# 例外処理を組み込んで例外に対処する
-begin
-  1 + "10"
-rescue
-   puts "例外が発生したが、このまま続行する"  
+def method_1
+  puts "method_1 start."
+  begin
+    method_2
+  rescue
+    puts "例外が発生しました。"
+  end
+  puts "method_1 end."
 end
 
-# 例外処理を組み込んだので、最後まで実行可能
-puts "End."
+# method_1をオーバーライドして例外処理を無くしたら、すべてのメソッドをさかのぼってエラーでプログラムが止まる
+def method_1
+  puts "method_1 start."
+  method_2
+  puts "method_1 end."
+end
+
+def method_2
+  puts "method_2 start."
+  method_3
+  puts "method_2 end."
+end
+
+def method_3
+  puts "method_3 start." 
+  # ZeroDivisionError (整数を⓪で除算しようとした場合に発生する例外)を発生させる
+  1/0
+  puts "method_3 end."
+end
+
+# 処理を開始
+method_1
